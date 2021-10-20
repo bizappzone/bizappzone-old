ee7c0f65bd73c6f2461309cfaca3d76ecc4acbc6
aj <art@ampsire.com>
2021-10-20 09:58:16 -0400

    add ui module to design app

--- apps/design-system/ngx-castle/src/app/app.module.ts
+++ apps/design-system/ngx-castle/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NgxCastleUiModule } from '@bizappzone/ngx-castle-ui';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    NgxCastleUiModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/design-system/ngx-castle-ui/src/lib/ngx-castle-ui.module.ts
+++ libs/design-system/ngx-castle-ui/src/lib/ngx-castle-ui.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button/button.component';
+import { BadgeComponent } from './badge/badge.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    ButtonComponent,
+    BadgeComponent
+  ],
+  exports: [
+    ButtonComponent,
+    BadgeComponent
+  ],
 })
 export class NgxCastleUiModule {}
